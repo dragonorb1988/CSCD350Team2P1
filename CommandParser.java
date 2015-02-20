13461d997ee8328997558b5a2dc2a57e40e86b68
Nicholas Valentine <navolas@gmail.com>
2015-02-19 21:27:25 -0800

    added some general parsing stuff
    Added in a bit of basic spliting.

--- CommandParser.java
+++ CommandParser.java
@@ -8,7 +8,35 @@
 
 import java.lang.String;
 
+import w15cs350.command.CommandManager;
+
 public class CommandParser
 {
    private String command;
+   
+   public CommandParser (String command, CommandManager manager)
+   {
+	  char firstChar = command.charAt(0);
+	  if (firstChar == '@')
+	  {
+		  //method to handle commands beginning with @s
+	  }
+	  else
+	  {
+		  String firstWord = command.substring(0, command.indexOf(' '));
+		  switch (firstWord)
+		  {
+		  		case "create": 
+		  			//call method to handle create commands
+		  			break;
+		  		case "set":
+		  			//call method to handle set commands
+		  			break;
+		  		default:
+		  			//handle other commands that are different
+		  			break;
+		  }
+		  	
+	  }
+   }
 }
